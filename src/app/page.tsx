255a2a095d533c0d76be3bc006f6e52ccbd21349
Andrew Virts <asvirts@outlook.com>
2023-10-27 16:43:28 -0500

    gifs secured

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
-import GetData from "./GetData";
+'use client'
 
+import { Grid } from '@giphy/react-components'
+import { GiphyFetch } from '@giphy/js-fetch-api'
 
 export default function Home() {
 
+  const gf = new GiphyFetch('MENNrEjZGJ5LyNYOw35WZX0LkglQNUSl')
+
+  const fetchGifs = (offset: number) => gf.trending({ offset, limit: 10 })
+
   return (
-    <main>
-      <GetData />
+    <main className='flex justify-center my-2'>
+      <Grid width={1800} columns={6} fetchGifs={fetchGifs} />
     </main>
   )
 }
