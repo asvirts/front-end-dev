70cf1cfab325e2e1119022039ff805554c574b1f
Andrew Virts <asvirts@outlook.com>
2023-10-30 18:55:06 -0500

    metadata

--- src/app/SearchGifs.tsx
+++ src/app/SearchGifs.tsx
@@ -1,10 +1,11 @@
 'use client'
 
 import React from 'react'
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import Image from 'next/image';
 
-async function QueryGifs(props) {
+async function QueryGifs(props: any) {
+
   let res = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_API_KEY}&q=${props.searchTerm}&limit=5&offset=0&rating=g&lang=en&bundle=messaging_non_clips`)
 
   const { data: gifs } = await res.json()
@@ -16,20 +17,30 @@ async function QueryGifs(props) {
   }
 
 export default function SearchGifs() {
-  const [search, setSearch] = useState('Search here');
-  
-  const handleChange = e => setSearch(e.target.value)
+  const [search, setSearch] = useState("Search here");
+  const inputRef = useRef()
+
+  function onSubmit(e: any) {
+    e.preventDefault()
+
+    const value = inputRef.current.value
+
+    if (value === "") return
+    setSearch(value)
+    inputRef.current.value = ""
+  }
 
   return (
     <div className='m-5'>
-      <input
-        className='m-2'
-        type="text"
-        placeholder='Search here'
-        value={search}
-      />
-      <button type="submit" className='bg-slate-700 text-white py-2 px-3 rounded'>Search</button>
-      
+      <form onSubmit={onSubmit}>
+        <input
+          ref={inputRef}
+          className='m-2'
+          type="search"
+          value={search}
+        />
+        <button type="submit" className='bg-slate-700 text-white py-2 px-3 rounded'>Search</button>
+      </form>
       <QueryGifs searchTerm={search} />
     </div>
   )
