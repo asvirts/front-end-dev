2fd5fc44a289e69f227aee8fb096bf513c30a02b
Andrew Virts <asvirts@outlook.com>
2023-11-01 11:44:58 -0500

    mp4 > iframe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,14 +8,14 @@ export default async function Page() {
 
     const { data: gifs } = await res.json()
 
-    let listGifs = gifs.map((gif: Gif), index: any => (
-      <div key={gif.data[index].id} className="text-white w-1/4 m-5 text-center">
-        <video src={gif.data[index].images.original.mp4}></video>
+    let listGifs = gifs.map((gif: Gif) => (
+      <div key={gif.id} className="text-white w-full m-5 text-center">
+        <video src={gif.images.original.mp4} autoPlay loop></video>
       </div>
     ))
 
     return (
-      <div className="m-5 grid grid-rows-3 grid-flow-col gap-4">{listGifs}</div>
+      <div className="m-5 grid grid-rows-1 grid-flow-col gap-4">{listGifs}</div>
     )
   }
 
